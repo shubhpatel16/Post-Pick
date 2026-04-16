4b649dfb8cbb7314807896b3598ac30f7c7f10cf
shubh patel <shubh16012002@gmail.com>
2026-04-16 18:02:23 +0530

    improve rating UI

--- frontend/src/components/Rating.jsx
+++ frontend/src/components/Rating.jsx
@@ -5,49 +5,50 @@ const Rating = ({ value, text, color }) => {
     <div className='rating'>
       <span>
         {value >= 1 ? (
-          <FaStar />
+          <FaStar color={color} />
         ) : value >= 0.5 ? (
-          <FaStarHalfAlt />
+          <FaStarHalfAlt color={color} />
         ) : (
-          <FaRegStar />
+          <FaRegStar color={color} />
         )}
       </span>
       <span>
         {value >= 2 ? (
-          <FaStar />
+          <FaStar color={color} />
         ) : value >= 1.5 ? (
-          <FaStarHalfAlt />
+          <FaStarHalfAlt color={color} />
         ) : (
-          <FaRegStar />
+          <FaRegStar color={color} />
         )}
       </span>
       <span>
         {value >= 3 ? (
-          <FaStar />
+          <FaStar color={color} />
         ) : value >= 2.5 ? (
-          <FaStarHalfAlt />
+          <FaStarHalfAlt color={color} />
         ) : (
-          <FaRegStar />
+          <FaRegStar color={color} />
         )}
       </span>
       <span>
         {value >= 4 ? (
-          <FaStar />
+          <FaStar color={color} />
         ) : value >= 3.5 ? (
-          <FaStarHalfAlt />
+          <FaStarHalfAlt color={color} />
         ) : (
-          <FaRegStar />
+          <FaRegStar color={color} />
         )}
       </span>
       <span>
         {value >= 5 ? (
-          <FaStar />
+          <FaStar color={color} />
         ) : value >= 4.5 ? (
-          <FaStarHalfAlt />
+          <FaStarHalfAlt color={color} />
         ) : (
-          <FaRegStar />
+          <FaRegStar color={color} />
         )}
       </span>
+
       <span className='rating-text'>{text && text}</span>
     </div>
   );
